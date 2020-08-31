d2e9f24951375d4e869bf05113b38975d6fece7a
David Cacuango <cacuangodarwin1999@gmail.com>
2020-08-31 22:48:25 +0000

    hice el deber 25

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 int main() 
 {
+  ofstream datos;
+  datos.open("datos.txt",ios::out);
   double salario, salariosemanal,salariototal;
   int trabajadores;
   cout << "ingrese el salario por dia de los trabajadores: ";
@@ -11,5 +14,8 @@ int main()
   salariosemanal=salario*5;
   salariototal=salariosemanal*trabajadores;
   cout << "el salario semanal es: " << salariosemanal << endl;
+  datos << "el salario semanal es: " << salariosemanal << endl;
   cout << "el salario total por los trabajadores es: "<<salariototal << endl;
+  datos << "el salario total por los trabajadores es: "<<salariototal << endl;
+  datos.close();
 }
